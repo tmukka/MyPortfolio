9530d31209d18685e765a4f677975d4238810ded
Tharun Mukka <Tharun.Mukka@mrcooper.com>
2018-10-31 12:25:11 +0530

    test commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div style={divcss}>
             <h1>Hi! I'm Tharun M. I'm a software developer based in Chennai</h1>
             <p>
-                <h3>I was currently working in Mr.Cooper as a FullStack Developer, mainly focusing on WCF Services and .NET Framewrok. I am an enthusiast of learning different technologies and working on them practically for better understanding of available use cases.</h3>
+                <h3>I am currently working in Mr.Cooper as a FullStack Developer, mainly focusing on WCF Services and .NET Framewrok. I am an enthusiast of learning different technologies and working on them practically for better understanding of available use cases.</h3>
             </p>
         </div>
     );
